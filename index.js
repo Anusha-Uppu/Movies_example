2f9a191d69d3b6f8153388142168fd1da29b10c5
Anusha Uppu <admin@Anusha-Uppu.local>
2024-07-02 13:01:22 +0530

    first table created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // import { hostname } from "os";
 
-const  {Sequelize}=require('sequelize');
+const  {Sequelize, DataTypes}=require('sequelize');
 
 const sequelize=new Sequelize('movies','anusha','anu@123',
     {hostname:'localhost',dialect:'postgres'})
@@ -10,3 +10,29 @@ sequelize.authenticate().then(()=>{
 }).catch((error)=>{
     console.log('Connection failed'+error);
 })
+
+const actor=sequelize.define(
+    'actor',
+    {
+        act_id:{
+            type: DataTypes.INTEGER,
+            allowNull:false,
+            primaryKey:true
+        },
+        act_fname:{
+            type:DataTypes.STRING
+        },
+        act_lname:{
+            type:DataTypes.STRING,
+        },
+        act_gender:{
+            type: DataTypes.STRING
+        }
+    }
+)
+ actor.sync({force :true}).then(()=>{
+    console.log('actor table created');
+ }).catch((error)=>{
+    console.log('actor table didnot created because'+error);
+ });
+
